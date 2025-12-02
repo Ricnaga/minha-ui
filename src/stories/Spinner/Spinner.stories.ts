c653a156b4e4287f305fd90d2ead7634d81e7735
ricnaga <ric.nagato@gmail.com>
2025-12-02 18:12:12 -0300

    chore: AJuste de arquitetura

--- src/stories/Spinner/Spinner.stories.ts
+++ src/stories/Spinner/Spinner.stories.ts
@@ -1,6 +1,5 @@
 import type { Meta, StoryObj } from "@storybook/react-vite";
-
-import { Spinner } from "../Spinner";
+import { Spinner } from "../../components";
 
 // More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
 const meta = {
