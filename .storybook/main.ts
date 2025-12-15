7092d1c77ee0453f3ed434cfc85e94e5bb00a81c
ricnaga <ric.nagato@gmail.com>
2025-12-15 16:50:23 -0300

    refactor: Add storybook config

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -3,7 +3,9 @@ import type { StorybookConfig } from "@storybook/react-vite";
 const config: StorybookConfig = {
   stories: [
     "../src/components/**/stories/*.stories.@(js|jsx|mjs|ts|tsx)",
-    "../src/types/**/stories/*.stories.@(js|jsx|mjs|ts|tsx)",
+    "../src/hooks/**/stories/*.stories.@(js|jsx|mjs|ts|tsx)",
+    // "../src/types/**/stories/*.stories.@(js|jsx|mjs|ts|tsx)",
+    // "../src/utils/**/stories/*.stories.@(js|jsx|mjs|ts|tsx)",
   ],
   addons: [
     "@chromatic-com/storybook",
