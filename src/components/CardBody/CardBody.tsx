8171effff54633ba0b2625291a071fe1d36c4aed
ricnaga <ric.nagato@gmail.com>
2025-12-18 14:31:54 -0300

    feat: add cardbody component

--- src/components/CardBody/CardBody.tsx
+++ src/components/CardBody/CardBody.tsx
@@ -1,3 +1,8 @@
-export function CardBody() {
-  return <div></div>;
+import type { CardBodyProps } from "./card-body.types";
+import { useCardBody } from "./useCardBody";
+
+export function CardBody(props: CardBodyProps) {
+  const { cardBodyProps } = useCardBody(props);
+
+  return <div {...cardBodyProps} />;
 }
