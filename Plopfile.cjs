13e2dd372df09993e69e40aeaf524e36d20b6524
ricnaga <ric.nagato@gmail.com>
2025-12-20 20:39:40 -0300

    feat: add plopfile | atualizar tailwind css e lock

---
+++ Plopfile.cjs
@@ -0,0 +1,34 @@
+module.exports = function (plop) {
+  plop.setGenerator("component", {
+    description: "Criar um componente React com hook e types",
+    prompts: [
+      {
+        type: "input",
+        name: "name",
+        message: "Nome do componente (ex: modal-header)?",
+      },
+    ],
+    actions: [
+      {
+        type: "add",
+        path: "src/components/{{pascalCase name}}/index.ts",
+        templateFile: "plop-templates/component/index.ts.hbs",
+      },
+      {
+        type: "add",
+        path: "src/components/{{pascalCase name}}/{{kebabCase name}}.types.ts",
+        templateFile: "plop-templates/component/types.ts.hbs",
+      },
+      {
+        type: "add",
+        path: "src/components/{{pascalCase name}}/{{pascalCase name}}.tsx",
+        templateFile: "plop-templates/component/component.tsx.hbs",
+      },
+      {
+        type: "add",
+        path: "src/components/{{pascalCase name}}/use{{pascalCase name}}.ts",
+        templateFile: "plop-templates/component/useHook.ts.hbs",
+      },
+    ],
+  });
+};
