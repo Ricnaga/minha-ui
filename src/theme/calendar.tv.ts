c227459f09625f7e5b591e86f7c8a26f171be6cb
ricnaga <ric.nagato@gmail.com>
2025-12-25 15:18:31 -0300

    style: atualizar estilo

--- src/theme/calendar.tv.ts
+++ src/theme/calendar.tv.ts
@@ -2,28 +2,32 @@ import { tv, type VariantProps } from "tailwind-variants";
 
 export const calendar = tv({
   slots: {
-    headerWrapper: ["flex items-center justify-between p-2"],
+    headerWrapper: ["flex items-center justify-between p-2 text-sm"],
     headerButton: [
-      "transition-all rounded-full bg-sky-50 p-2 hover:bg-sky-100 cursor-pointer",
+      "transition-all rounded-full",
+      "bg-sky-50 p-1 hover:bg-sky-100 cursor-pointer ",
     ],
     headerLabel: ["capitalize font-medium"],
     gridWrapper: ["grid grid-cols-7 gap-1 p-2"],
-    gridDay: ["size-8 rounded-full text-sm transition-colors cursor-pointer"],
+    gridDay: [
+      "size-6 rounded-full text-sm transition-colors cursor-pointer",
+      "hover:bg-sky-400/20"
+    ],
   },
   variants: {
     isSelected: {
       true: {
-        gridDay: "bg-sky-400 text-white",
+        gridDay: "bg-sky-400 text-white hover:bg-sky-500",
       },
     },
     hasNotSelectedAndIsOutsideMonth: {
       true: {
-        gridDay: "text-neutral-400 opacity-40",
+        gridDay: "text-neutral-400 opacity-40 ",
       },
     },
     hasNotSelectedAndHasNotOutsideMonth: {
       true: {
-        gridDay: "text-neutral-400 opacity-40",
+        gridDay: "text-neutral-400 opacity-40 ",
       },
     },
   },
