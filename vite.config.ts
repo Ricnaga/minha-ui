9313995236e64a221eeeb18ffce4b54df52ee359
ricnaga <ric.nagato@gmail.com>
2025-12-29 17:35:14 -0300

    feat: Add vite path alias

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,8 @@ import { fileURLToPath } from "node:url";
 import { storybookTest } from "@storybook/addon-vitest/vitest-plugin";
 import { playwright } from "@vitest/browser-playwright";
 import tailwindcss from "@tailwindcss/vite";
+import tsconfigPaths from 'vite-tsconfig-paths'
+
 const dirname =
   typeof __dirname !== "undefined"
     ? __dirname
@@ -15,7 +17,7 @@ const dirname =
 
 // More info at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(), tsconfigPaths()],
   test: {
     projects: [
       {
