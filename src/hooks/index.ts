fc06951069e3a845b6810dec3e56b427af3c7b81
ricnaga <ric.nagato@gmail.com>
2025-12-29 14:44:16 -0300

    feat: add melhoria do use contenxt hook

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,3 +1,5 @@
+export * from "./useClickOutside";
 export * from "./useContext";
+export * from "./useFetch";
 export * from "./useRipple";
 export * from "./useToggle";
---
+++ src/hooks/useContext/stories/useContext.stories.tsx
@@ -0,0 +1,55 @@
+import { createContext, useState } from "react";
+import { useContext } from "..";
+import type { RequiredChildren } from "../../../types";
+
+export default {
+  title: "Hooks/useContext",
+  parameters: {
+    layout: "centered",
+  },
+  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
+  tags: ["autodocs"],
+};
+
+// Context de teste apenas para o story
+type DemoContextType = { value: string; setValue: (v: string) => void };
+const DemoContext = createContext<DemoContextType | undefined>(undefined);
+
+export const Default = () => {
+  // Provider minimalista para o story
+  const DemoProvider = ({ children }: RequiredChildren) => {
+    const [value, setValue] = useState("Hello Storybook!");
+    return (
+      <DemoContext.Provider value={{ value, setValue }}>
+        {children}
+      </DemoContext.Provider>
+    );
+  };
+
+  // Usando o hook
+  const DemoConsumer = () => {
+    const { value, setValue } = useContext({
+      context: DemoContext,
+      hookName: "useDemoContext",
+      providerName: "Demo",
+    });
+
+    return (
+      <div className="text-center">
+        <p>Value from context: {value}</p>
+        <button
+          className="text-white bg-blue-500 rounded p-1 cursor-pointer"
+          onClick={() => setValue("Updated value!")}
+        >
+          Update Value
+        </button>
+      </div>
+    );
+  };
+
+  return (
+    <DemoProvider>
+      <DemoConsumer />
+    </DemoProvider>
+  );
+};
