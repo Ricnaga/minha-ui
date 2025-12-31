3dcec141132c0c18d634c04ac6574d3952847fc0
ricnaga <ric.nagato@gmail.com>
2025-12-31 19:16:20 -0300

    chore: Add autodocs

--- src/components/Calendar/stories/Calendar.stories.tsx
+++ src/components/Calendar/stories/Calendar.stories.tsx
@@ -11,6 +11,8 @@ const meta: Meta<CalendarProps> = {
     // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
     layout: "centered",
   },
+  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
+  tags: ["autodocs"],
   args: {
     locale: "pt-BR",
   },
