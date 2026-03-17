8aa522b47ddfc0f8f4a66ee7a0b4d19fbf122c31
ricnaga <ric.nagato@gmail.com>
2026-03-17 17:54:03 -0300

    chore: Remover linha inutilizada

--- src/hooks/useResource/stories/useResource.stories.tsx
+++ src/hooks/useResource/stories/useResource.stories.tsx
@@ -1,4 +1,4 @@
-/* eslint-disable react-hooks/rules-of-hooks */
+ 
 import type { Meta, StoryObj } from "@storybook/react-vite";
 import { Suspense } from "react";
 import { useResource } from "../";
