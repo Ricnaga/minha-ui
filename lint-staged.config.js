d8cab0a98b09edea4c57b9550cba137b5ea68325
ricnaga <ric.nagato@gmail.com>
2026-03-19 11:21:20 -0300

    feat(ci): add unit tests to lint-staged
    - Add test:unit --bail=1 to pre-commit hooks
    - Ignore __snapshots__ directory in git

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,4 +1,4 @@
 export default {
-  "*.{ts,tsx}": ["eslint . --fix", "pnpm typecheck"],
-  "*.{js,jsx,ts,tsx,json,css}": ["prettier --write"],
+  '*.{ts,tsx}': ['eslint . --fix', 'pnpm typecheck', 'pnpm test:unit --bail=1'],
+  '*.{js,jsx,ts,tsx,json,css}': ['prettier --write'],
 };
